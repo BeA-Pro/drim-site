127be245a92ced6d504aa15908a2964d63ae0539
YongHyunKing <yhkim8246@gmail.com>
2024-01-11 15:20:35 +0900

    feat: add test image and logo

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 // scss
 import "../../asset/scss/header.scss";
 
+//images
+import Logo from "../../asset/images/Logo.png";
+
 function Header() {
 
   return (
     <div className="header_wrap">
-
+      <img src={Logo} alt="Logo" className="Logo" />
     </div>
   )
 
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,16 +1,18 @@
 import React from "react";
+// scss
+import "../../asset/scss/main.scss";
+
+// images
+import Main_image1 from '../../asset/images/main_image1.png';
 
 
 function Main() {
-
   return (
-    <>
-
-    </>
+    <div className="main-wrap">
+      <img className="main-image" src={Main_image1} alt="메인이미지" />
+    </div>
   )
 
 }
-
-
 
 export default Main;
