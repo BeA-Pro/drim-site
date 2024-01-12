b5f9e7eff11b5e91a6e2bebb4ccc067ebaf2d537
YongHyunKing <yhkim8246@gmail.com>
2024-01-12 12:54:11 +0900

    feat : add main img with full width

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -9,7 +9,9 @@ import Main_image1 from '../../asset/images/main_image1.png';
 function Main() {
   return (
     <>
-
+      <img className="main_image" href="메인 이미지" src={Main_image1} />
+      <div className="dump">
+      </div>
     </>
   )
 
