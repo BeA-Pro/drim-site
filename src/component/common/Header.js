f5b465a707a77aa5636eea079ae84324111d57a4
YongHyunKing <yhkim8246@gmail.com>
2024-01-15 21:36:32 +0900

    feat: Implement basic site header

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // scss
 import "../../asset/scss/header.scss";
 
@@ -7,18 +7,74 @@ import Logo from "../../asset/images/Logo.png";
 
 function Header() {
 
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    console.log('hi');
+    setMenuOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMenuOpen(false);
+  };
+
+
   return (
     <div className="header_wrap">
-      <div className="header_Logo">로고</div>
-      <div className="menu">
-        <ul>
-          <li>회사 소개</li>
-          <li>사업 소개</li>
-          <li>부동산 보고서</li>
-          <li>시공 문의</li>
-        </ul>
+      <div className="menu-top">
+        <h1>Logo</h1>
+        <nav className="menu">
+          <ul onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+            <li>
+              <button>회사소개</button>
+            </li>
+            <li>
+              <button>사업소개</button>
+
+            </li>
+            <li>
+              <button>부동산보고서</button>
+            </li>
+            <li>
+              <button>시공문의</button>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </div>
+
+      <div className='menu-bottom'>
+        <ul className={`${isMenuOpen ? 'hovered' : ''}`}>
+          <li>
+            경영이념
+          </li>
+          <li>
+            인사말
+          </li>
+          <li>
+            연혁
+          </li>
+          <li>
+            오시는 길
+          </li>
+        </ul>
+        <ul className={`${isMenuOpen ? 'hovered' : ''}`}>
+          <li>건축사업</li>
+          <li>리모델링사업</li>
+          <li>인테리어사업</li>
+        </ul>
+        <ul className={`${isMenuOpen ? 'hovered' : ''}`}>
+          <li>부동산보고서</li>
+        </ul>
+        <ul className={`${isMenuOpen ? 'hovered' : ''}`}>
+          <li>시공문의</li>
+        </ul>
+
+
+      </div>
+
+
+    </div >
   )
 
 }
