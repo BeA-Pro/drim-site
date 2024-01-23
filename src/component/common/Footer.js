a9ee53109d4c8b023bd7e75e3927fa23991a249e
YongHyunKing <yhkim8246@gmail.com>
2024-01-23 19:26:43 +0900

    feat: add logo and change footer design

--- src/component/common/Footer.js
+++ src/component/common/Footer.js
@@ -2,24 +2,28 @@ import React from "react";
 //scss
 import "../../asset/scss/footer.scss";
 
+// Logo
+import Logo from "../../asset/icons/drim-logo.svg";
+
 function Footer() {
 
   return (
     <div className="footer_wrap">
-      <h1>
-        Logo
-      </h1>
-      <ul className="footer_info">
-        <li>
-          <a>주소 (10894) 경기도 파주시 금바위로42, 10층 1009호 | 대표전화 : 010-7367-7896 | 상호 : 드림종합건설 | 개인정보처리방침 / 위치정보사업이용약관</a>
-        </li>
-      </ul>
-      <ul className="footer_copyright">
-        <li>
-          <a>Copyright©2002-2020 DRIM. All Rights Reserved</a>
-        </li>
-      </ul>
+      <div className="footer-inner">
+        <img src={Logo} alt="Logo" className="Logo" />
+        <ul className="footer_info">
+          <li>
+            <a>주소 (10894) 경기도 파주시 금바위로42, 10층 1009호 | 대표전화 : 010-7367-7896 | 상호 : 드림종합건설 | 개인정보처리방침 / 위치정보사업이용약관</a>
+          </li>
+        </ul>
+        <ul className="footer_copyright">
+          <li>
+            <a>Copyright©2002-2020 DRIM. All Rights Reserved</a>
+          </li>
+        </ul>
+      </div>
     </div>
+
   )
 }
 
