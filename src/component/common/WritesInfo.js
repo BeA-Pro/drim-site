c5de0b84af3e105d0c57b330fdac7a8729ad41bb
YongHyunKing <yhkim8246@gmail.com>
2024-01-29 18:55:54 +0900

    feat: add animation to third section

--- src/component/common/WritesInfo.js
+++ src/component/common/WritesInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 // scss
 import "../../asset/scss/writesInfo.scss"
@@ -6,23 +6,49 @@ import "../../asset/scss/writesInfo.scss"
 
 function WritesInfo() {
 
+  const [isVisible, setIsVisible] = useState(false);
+
+  const elementRef = useRef(null);
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const element = elementRef.current;
+      if (!element) return;
+
+      const elementBottom = element.getBoundingClientRect().bottom;
+      const windowHeight = window.innerHeight;
+
+      if (elementBottom < windowHeight) {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+  }, []);
+
+
 
   return (
-    <div className="writeInfo-wrap">
-      <div className="writeInfo-first">
-        <div className="writeInfo-first title">Environmental</div>
-        <div className="writeInfo-first subTitle">leadership is out DNA</div>
-        <div className="writeInfo-first detail">Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
+    <div className="writeInfo-wrap" ref={elementRef}>
+      <div className={`writeInfo-first ${isVisible ? 'hover' : ''}`}>
+        <div className={`writeInfo-first title ${isVisible ? 'hover' : ''}`}>Environmental</div>
+        <div className={`writeInfo-first subTitle ${isVisible ? 'hover' : ''}`}>leadership is out DNA</div>
+        <div className={`writeInfo-first detail ${isVisible ? 'hover' : ''}`}>Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
       </div>
-      <div className="writeInfo-second">
-        <div className="writeInfo-second title">Social</div>
-        <div className="writeInfo-second subTitle">equity is good design</div>
-        <div className="writeInfo-second detail">Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
+      <div className={`writeInfo-second ${isVisible ? 'hover' : ''}`}>
+        <div className={`writeInfo-second title ${isVisible ? 'hover' : ''}`}>Social</div>
+        <div className={`writeInfo-second subTitle ${isVisible ? 'hover' : ''}`}>equity is good design</div>
+        <div className={`writeInfo-second detail ${isVisible ? 'hover' : ''}`}>Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
       </div>
-      <div className="writeInfo-third">
-        <div className="writeInfo-third title">Governance</div>
-        <div className="writeInfo-third subTitle">reinforces our values</div>
-        <div className="writeInfo-third detail">Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
+      <div className={`writeInfo-third ${isVisible ? 'hover' : ''}`}>
+        <div className={`writeInfo-third title ${isVisible ? 'hover' : ''}`}>Governance</div>
+        <div className={`writeInfo-third subTitle ${isVisible ? 'hover' : ''}`}>reinforces our values</div>
+        <div className={`writeInfo-third detail ${isVisible ? 'hover' : ''}`}>Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
       </div>
     </div >
   )
