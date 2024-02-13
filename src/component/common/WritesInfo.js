ecb8fa051176a345dac40d0ce6a0b527bb9b19a6
YongHyunKing <yhkim8246@gmail.com>
2024-02-13 13:02:49 +0900

    feat: string js

--- src/component/common/WritesInfo.js
+++ src/component/common/WritesInfo.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect, useRef } from 'react';
 // scss
 import "../../asset/scss/writesInfo.scss"
 
+// strings
+import { environment, environmentSubtitle, environmentWrites, social, socialSubtitle, socialWrites, goveranance, goverananceSubtitle, goverananceWrites } from '../../component/data/string'
+
 
 function WritesInfo() {
 
@@ -36,19 +39,19 @@ function WritesInfo() {
   return (
     <div className="writeInfo-wrap" ref={elementRef}>
       <div className={`writeInfo-first wrap ${isVisible ? 'hover' : ''}`}>
-        <div className={`writeInfo-first title ${isVisible ? 'hover' : ''}`}>Environmental</div>
-        <div className={`writeInfo-first subTitle ${isVisible ? 'hover' : ''}`}>leadership is out DNA</div>
-        <div className={`writeInfo-first detail ${isVisible ? 'hover' : ''}`}>Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
+        <div className={`writeInfo-first title ${isVisible ? 'hover' : ''}`}>{environment}</div>
+        <div className={`writeInfo-first subTitle ${isVisible ? 'hover' : ''}`}>{environmentSubtitle}</div>
+        <div className={`writeInfo-first detail ${isVisible ? 'hover' : ''}`}>{environmentWrites}</div>
       </div>
       <div className={`writeInfo-second wrap ${isVisible ? 'hover' : ''}`}>
-        <div className={`writeInfo-second title ${isVisible ? 'hover' : ''}`}>Social</div>
-        <div className={`writeInfo-second subTitle ${isVisible ? 'hover' : ''}`}>equity is good design</div>
-        <div className={`writeInfo-second detail ${isVisible ? 'hover' : ''}`}>Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
+        <div className={`writeInfo-second title ${isVisible ? 'hover' : ''}`}>{social}</div>
+        <div className={`writeInfo-second subTitle ${isVisible ? 'hover' : ''}`}>{socialSubtitle}</div>
+        <div className={`writeInfo-second detail ${isVisible ? 'hover' : ''}`}>{socialWrites}</div>
       </div>
       <div className={`writeInfo-third wrap ${isVisible ? 'hover' : ''}`}>
-        <div className={`writeInfo-third title ${isVisible ? 'hover' : ''}`}>Governance</div>
-        <div className={`writeInfo-third subTitle ${isVisible ? 'hover' : ''}`}>reinforces our values</div>
-        <div className={`writeInfo-third detail ${isVisible ? 'hover' : ''}`}>Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words Write some words</div>
+        <div className={`writeInfo-third title ${isVisible ? 'hover' : ''}`}>{goveranance}</div>
+        <div className={`writeInfo-third subTitle ${isVisible ? 'hover' : ''}`}>{goverananceSubtitle}</div>
+        <div className={`writeInfo-third detail ${isVisible ? 'hover' : ''}`}>{goverananceWrites}</div>
       </div>
     </div >
   )
---
+++ src/component/data/string.js
@@ -0,0 +1,17 @@
+
+// section 4
+export const environment = "환경 (Environment)";
+export const environmentSubtitle = "환경을 위한 건설";
+export const environmentWrites = "우리는 친환경 자재 및 기술을 적극 도입하여,\n에너지 효율성을 높이고 탄소 배출을 최소화합니다.";
+
+export const social = "사회 (Social)";
+export const socialSubtitle = "지역사회와 협력";
+export const socialWrites = "사회적 책임을 다하는 일원으로서 지역사회에\n긍정적인 영향을 주기 위해 노력합니다.";
+
+
+export const goveranance = "지배구조 (Governance)";
+export const goverananceSubtitle = " 투명성, 정직, 지속 가능한 성장";
+export const goverananceWrites =
+  "투명한 의사결정 및 회계 체계를 유지하고\n윤리적 경영 원칙을 준수합니다."
+
+// "투명한 의사결정 및 회계 체계를 유지하여 주주와 이해관계자들에게 신뢰와 확신을 제공하고 윤리적 경영 원칙을 준수하며, 회사의 지속 가능한 성장을 위해 적절한 지배구조를 구축하고 유지합니다."
