262f69d6b3303ecd86324d74592dbafcd2d78b6e
YongHyunKing <yhkim8246@gmail.com>
2024-09-02 16:47:02 +0900

    fix: 모바일 푸터 내용 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,12 +35,12 @@ function MobileFooter() {
         <img src={Logo} alt="Logo" className="Logo" />
         <ul className="footer-info">
           <li>
-            주소 (10894) 경기도 파주시 금바위로42, 10층 1009호 |<br /> 대표전화 : 010-7367-7896 | 상호 : 드림종합건설 | 개인정보처리방침 / 위치정보사업이용약관
+            {footer1}
           </li>
         </ul>
         <ul className="footer-copyright">
           <li>
-            Copyright©2002-2020 DRIM. All Rights Reserved
+            {footer2}
           </li>
         </ul>
       </div>
